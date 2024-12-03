82dc5da9fc7e28e3f57ef63394231abb0cef0b8a
penz6 <thetechkid66@gmail.com>
2024-12-03 11:56:19 -0500

    update 2

--- runner.py
+++ runner.py
@@ -13,8 +13,14 @@ import statistics
 # data from json, treat line as object
 data = pd.read_json("data1.json",lines=True)
 
+#get populairty data
+print(data['popularity'].describe)
+print("Mean:"+statistics.mean(data['popularity']))
+print("Deviation:"+statistics.stdev(data['popularity']))
 #clean data
-data = data.drop(data['popularity'<750].index)
+data = data.drop(data[data['popularity']<750].index)
+data['episode_run_time'] = data['episode_run_time'].apply(lambda x: x[0] if isinstance(x, list) and x else None)
+
 #print(data)
 
 #multi threaded plotting
@@ -34,7 +40,8 @@ def multi_plot(arg):
     plt.close()
 
 #what to plot
-plotting_data = [[data['first_air_date'],data['popularity'],'X: Air Date | Y:Populairty'],[statistics.mean(data['episode_run_time']),data['popularity'],"X:Runtime | Y:Popularity"],[data['vote_count'],data["popularity"],"X:Vote Count | Y:Popularity"],[data['popularity'],data["vote_average"],"'X: Popularity | Y:Vote Score'"],[data['languages'],data['popularity'],"X: Language | Y: Popularity"]]
+plotting_data = [[data['first_air_date'],data['popularity'],'X_Air Date_YPopulairty'],[data['episode_run_time'],data['popularity'],"XRuntime_YPopularity"],[data['vote_count'],data["popularity"],"XVote_Count_YPopularity"],[data['popularity'],data["vote_average"],'X_Popularity_YVote Score'],[data['languages'],data['popularity'],"X_Language_Y_Popularity"]]
+
 #multi_plot(plotting_data[0])
 if __name__ == '__main__':
     #cpus
